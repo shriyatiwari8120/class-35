0ebd463cc08815a0005764d6b24063f22b131b80
shriyaTiwari <shriyatravel2018@gmail.com>
2021-04-05 15:33:27 +0530

    class 35

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 var ball;
+var dataBase;
+var position;
 
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    dataBase = firebase.database()
+    var positiondb = dataBase.ref("pos")
+    positiondb.on("value",readPosition)
+
 }
 
 function draw(){
@@ -24,6 +31,21 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+   // ball.x = ball.x + x;
+  // ball.y = ball.y + y;
+ 
+  dataBase.ref("pos").set(
+    {
+        x:position.x+x,
+        y:position.y+y
+    }
+  )
+}
+
+
+function readPosition(data){
+    position=data.val();
+
+    ball.x= position.x;
+    ball.y= position.y;
 }
